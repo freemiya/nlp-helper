372f3b91c4da26d3c6b8ad67e5df7eab5079b6f9
gitlost-murali <kmanoharmurali@gmail.com>
2020-07-22 20:21:30 +0530

    Codebase update: `deprecated/`:Current Flask based app moved to `deprecated`. Will be replicated using Streamlit

--- augmentation_apps/app.py
+++ augmentation_apps/app.py
@@ -1,62 +1,17 @@
-import flask
-from flask import Flask, request, render_template
+import streamlit as st
 import json
 import main
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-# @app.route('/get_end_predictions', methods=['post'])
-# def get_prediction_eos():
-#     try:
-#         input_text = ' '.join(request.json['input_text'].split())
-#         input_text += ' <mask>'
-#         top_k = request.json['top_k']
-#         res = main.get_all_predictions(input_text, top_clean=int(top_k))
-#         return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
-#     except Exception as error:
-#         err = str(error)
-#         print(err)
-#         return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
-
-
-# @app.route('/get_prediction_mask', methods=['get, post'])
-# def get_prediction_mask():
-#     try:
-#         input_text = ' '.join(request.json['input_text'].split())
-#         top_k = request.json['top_k']
-#         res = main.get_mask_predictions(input_text, top_clean=int(top_k))
-#         print(res)
-#         return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
-#     except Exception as error:
-#         err = str(error)
-#         print(err)
-#         return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
-
-@app.route('/augmentation')
-def augmentation():
-    return render_template('augmentation.html')
-
-@app.route('/get_predictions', methods=['post'])
+st.title("Data Augmentation")
+@st.cache
 def get_predictions():
-    input_text = ' '.join(request.json['input_text'].split())
-    top_k = request.json['top_k']
-    res = main.get_mask_predictions(input_text)
-
     try:
         input_text = ' '.join(request.json['input_text'].split())
-        top_k = request.json['top_k']
-        res = main.get_mask_predictions(input_text)
-        return app.response_class(response=json.dumps(res), status=200, mimetype='application/json')
+        top_k = int(request.json['top_k'])
+        res = main.get_mask_predictions(input_text, top_k)
+        return json.dumps(res)
     except Exception as error:
         err = str(error)
-        print(err)
-        return app.response_class(response=json.dumps(err), status=500, mimetype='application/json')
+        return json.dumps(err)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8000, use_reloader=False)
+ 
